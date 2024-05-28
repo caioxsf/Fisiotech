356f7df4299492f4a3b8f101b97c903913259447
caioxsf <caio.nobile@hotmail.com>
2024-05-28 13:42:09 -0300

    cadastro de paciente totalmente validado (todos os campos e tabela dinamica feita!)

--- assets/js/validacoes.js
+++ assets/js/validacoes.js
@@ -1,183 +1,184 @@
 // Os comentários estão sendo referidos as validações do consulta.html. Foram usadas as mesmas funções para o paciente.html e index.html(contato).
 
 // validarNomes() usado para fazer a verificação do campo Nome completo, Cidade e Bairro.
-// function validarNomes ()
-// {
-//     var nome = event.target.value;
-//     var arr = nome.split(" ");
+function validarNomes ()
+{
+    var nome = event.target.value;
+    var arr = nome.split(" ");
 
-//     if(arr[1])
-//     {
-//         event.target.style.border= "1px solid green";
-//         return true;
-//     }
-//     else
-//     {
-//         event.target.style.border = "1px solid red";
-//         event.target.setAttribute('placeholder', 'Digite o campo corretamente!');
-//         event.target.value = "";
-//     }   
-// }
+    if(arr[1])
+    {
+        event.target.style.border= "1px solid green";
+        return true;
+    }
+    else
+    {
+        event.target.style.border = "1px solid red";
+        event.target.setAttribute('placeholder', 'Digite o campo corretamente!');
+        event.target.value = "";
+    }   
+}
 
-// // mascara() usado para fazer a verificação do campo Telefone, CPF e CEP.
-// function mascara(m,t,e){
-//     var cursor = t.selectionStart;
-//     var texto = t.value;
-//     texto = texto.replace(/\D/g,'');
-//     var l = texto.length;
-//     var lm = m.length;
-//     if(window.event) {                  
-//        id = e.keyCode;
-//     } else if(e.which){                 
-//        id = e.which;
-//     }
-//     cursorfixo=false;
-//     if(cursor < l)cursorfixo=true;
-//     var livre = false;
-//     if(id == 16 || id == 19 || (id >= 33 && id <= 40))livre = true;
-//     ii=0;
-//     mm=0;
-//     if(!livre){
-//        if(id!=8){
-//           t.value="";
-//           j=0;
-//           for(i=0;i<lm;i++){
-//              if(m.substr(i,1)=="#"){
-//                 t.value+=texto.substr(j,1);
-//                 j++;
-//              }else if(m.substr(i,1)!="#"){
-//                       t.value+=m.substr(i,1);
-//                     }
-//                     if(id!=8 && !cursorfixo)cursor++;
-//                     if((j)==l+1)
-//                         break;    
-//           } 	
-//        }
-//     }
-//     if(cursorfixo && !livre)cursor--;
-//       t.setSelectionRange(cursor, cursor);
+// mascara() usado para fazer a verificação do campo Telefone, CPF e CEP.
+function mascara(m,t,e){
+    var cursor = t.selectionStart;
+    var texto = t.value;
+    texto = texto.replace(/\D/g,'');
+    var l = texto.length;
+    var lm = m.length;
+    if(window.event) {                  
+       id = e.keyCode;
+    } else if(e.which){                 
+       id = e.which;
+    }
+    cursorfixo=false;
+    if(cursor < l)cursorfixo=true;
+    var livre = false;
+    if(id == 16 || id == 19 || (id >= 33 && id <= 40))livre = true;
+    ii=0;
+    mm=0;
+    if(!livre){
+       if(id!=8){
+          t.value="";
+          j=0;
+          for(i=0;i<lm;i++){
+             if(m.substr(i,1)=="#"){
+                t.value+=texto.substr(j,1);
+                j++;
+             }else if(m.substr(i,1)!="#"){
+                      t.value+=m.substr(i,1);
+                    }
+                    if(id!=8 && !cursorfixo)cursor++;
+                    if((j)==l+1)
+                        break;    
+          } 	
+       }
+    }
+    if(cursorfixo && !livre)cursor--;
+      t.setSelectionRange(cursor, cursor);
 
-//     var l = texto.length;
-//     if (j > l)
-//     {
-//         event.target.style.border = "1px solid red";
-//         event.target.setAttribute('placeholder', 'Digite o campo corretamente!');
-//         event.target.value = "";
-//     }
-//     else
-//     {
-//         t.style.border = "1px solid green";
-//     }
-//   }
+    var l = texto.length;
+    if (j > l)
+    {
+        event.target.style.border = "1px solid red";
+        event.target.setAttribute('placeholder', 'Digite o campo corretamente!');
+        event.target.value = "";
+    }
+    else
+    {
+        t.style.border = "1px solid green";
+    }
+  }
 
-// // validarEmail() usado para fazer a verificação do campo Email.
-// function validarEmail ()
-// {
-//     var email = event.target.value;
-//     var regEx = new RegExp(/^(\w+[\-\.])*\w+@(\w+\.)+[A-Za-z]+$/); 
+// validarEmail() usado para fazer a verificação do campo Email.
+function validarEmail ()
+{
+    var email = event.target.value;
+    var regEx = new RegExp(/^(\w+[\-\.])*\w+@(\w+\.)+[A-Za-z]+$/); 
 
-//     var validacaoEmail = regEx.test(email);
+    var validacaoEmail = regEx.test(email);
 
-//     if(validacaoEmail)
-//         event.target.style.border= "1px solid green";
-//     else
-//     {
-//         event.target.style.border = "1px solid red";
-//         event.target.setAttribute('placeholder', 'Digite o campo corretamente!');
-//         event.target.value = "";
-//     }   
-// }
+    if(validacaoEmail)
+        event.target.style.border= "1px solid green";
+    else
+    {
+        event.target.style.border = "1px solid red";
+        event.target.setAttribute('placeholder', 'Digite o campo corretamente!');
+        event.target.value = "";
+    }   
+}
 
-// // validarData() usado para fazer a verificação do campo Data de nascimento.
-// function validarData ()
-// {
-//     var data = event.target.value;
-//     var arr = data.split("-");
-//     var op=true;
+// validarData() usado para fazer a verificação do campo Data de nascimento.
+function validarData ()
+{
+    var data = event.target.value;
+    var arr = data.split("-");
+    var op=true;
 
-//     var dataAtual = new Date();
-//     var dia = dataAtual.getDate();
-//     var mes = dataAtual.getMonth();
-//     var ano = dataAtual.getFullYear();
+    var dataAtual = new Date();
+    var dia = dataAtual.getDate();
+    var mes = dataAtual.getMonth();
+    var ano = dataAtual.getFullYear();
+    var anoAntigo = ano - 105;
 
-//     console.log(arr);
-//     if(arr[0] == '')
-//     {
-//         event.target.style.border = "1px solid red";
-//         op=false;
-//     }
-//     else
-//     {
-//         if(arr[0] > ano)
-//         {
-//             event.target.style.border = "1px solid red";
-//             event.target.value = "";
-//             op = false;
-//         }
-//         else
-//         {
-//             if(arr[0] == ano)
-//             {
-//                 if(arr[2] > dia)
-//                 {
-//                     if(arr[1] > mes)
-//                     {
-//                         event.target.style.border= "1px solid red";
-//                         event.target.value = "";
-//                         op = false;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//     if(op == true)
-//         event.target.style.border= "1px solid green";
+    console.log(anoAntigo);
+    if(arr[0] == '')
+    {
+        event.target.style.border = "1px solid red";
+        op=false;
+    }
+    else
+    {
+        if(arr[0] > ano || arr[0] < anoAntigo)
+        {
+            event.target.style.border = "1px solid red";
+            event.target.value = "";
+            op = false;
+        }
+        else
+        {
+            if(arr[0] == ano)
+            {
+                if(arr[2] > dia)
+                {
+                    if(arr[1] > mes)
+                    {
+                        event.target.style.border= "1px solid red";
+                        event.target.value = "";
+                        op = false;
+                    }
+                }
+            }
+        }
+    }
+    if(op == true)
+        event.target.style.border= "1px solid green";
     
-// }
+}
 
-// //validarCidade() usado para fazer a verificação do campo Cidade
-// function validarCidade ()
-// {
-//     var cidade = event.target.value;
+//validarCidade() usado para fazer a verificação do campo Cidade
+function validarCidade ()
+{
+    var cidade = event.target.value;
 
-//     if(cidade)
-//         event.target.style.border= "1px solid green";
-//     else
-//     {
-//         event.target.style.border= "1px solid red";
-//         event.target.setAttribute('placeholder', 'Digite o campo corretamente!');
-//         event.target.value = "";
-//     }
+    if(cidade)
+        event.target.style.border= "1px solid green";
+    else
+    {
+        event.target.style.border= "1px solid red";
+        event.target.setAttribute('placeholder', 'Digite o campo corretamente!');
+        event.target.value = "";
+    }
         
-// }
+}
 
-// // borda(select) usado para fazer a verificação do campo Sexo e Estado.
-// function borda (select)
-// {
-//     var opSelecionada = select.value;
+// borda(select) usado para fazer a verificação do campo Sexo e Estado.
+function borda (select)
+{
+    var opSelecionada = select.value;
 
-//     if(opSelecionada != "Selecione uma opção")
-//         select.style.border= "1px solid green";
-//     else
-//         select.style.border= "1px solid red";
-// }
+    if(opSelecionada != "Selecione uma opção")
+        select.style.border= "1px solid green";
+    else
+        select.style.border= "1px solid red";
+}
 
-// // paciente.html
-// function validarHora ()
-// {
-//     var hora = event.target.value;
+// paciente.html
+function validarHora ()
+{
+    var hora = event.target.value;
     
-//     var horaArr = hora.split(":");
+    var horaArr = hora.split(":");
 
-//     if(horaArr[0] && horaArr[1])
-//         event.target.style.border= "1px solid green";
-//     else
-//     {
-//         event.target.style.border= "1px solid red";
-//         event.target.value = "";
-//     }
+    if(horaArr[0] && horaArr[1])
+        event.target.style.border= "1px solid green";
+    else
+    {
+        event.target.style.border= "1px solid red";
+        event.target.value = "";
+    }
         
-// }
+}
 
 var listaPaciente = [];
 
@@ -195,8 +196,8 @@ function validar ()
     var vestado = document.getElementById('estado').value;
     var vcep = document.getElementById('cep').value;
 
-    // if(vnome && vtelefone && vemail && vnascimento && vcpf && vendereco && vbairro && vsexo && vcidade && vestado && vcep)
-    // {
+    if(vnome && vtelefone && vemail && vnascimento && vcpf && vendereco && vbairro && vsexo && vcidade && vestado && vcep)
+    {
         listaPaciente.push ({
             nome: vnome,
             telefone: vtelefone,
@@ -211,12 +212,67 @@ function validar ()
             cep: vcep
         });
         adicionarItem(listaPaciente);
-        document.getElementById('display-fundo').style.display = 'block';
-    // }
-    // else
-    //     console.log('erro'); 
+
+    }
+    else
+    {
+        if(vnome == "")
+        {
+            document.getElementById('nome').style.border = "1px solid red";
+            document.getElementById('nome').setAttribute('placeholder', 'Digite o nome corretamente!');
+        }
+        if(vtelefone == "")
+        {
+            document.getElementById('telefone').style.border = "1px solid red";
+            document.getElementById('telefone').setAttribute('placeholder', 'Digite o telefone corretamente!');
+        }
+        if(vemail == "")
+        {
+            document.getElementById('email').style.border = "1px solid red";
+            document.getElementById('email').setAttribute('placeholder', 'Digite o email corretamente!');
+        }
+        if(vnascimento == "")
+        {
+            document.getElementById('nascimento').style.border = "1px solid red";
+        }
+        if(vcpf == "")
+        {
+            document.getElementById('cpf').style.border = "1px solid red";
+            document.getElementById('cpf').setAttribute('placeholder', 'Digite o CPF corretamente!');
+        }
+        if(vendereco == "")
+        {
+            document.getElementById('endereco').style.border = "1px solid red";
+            document.getElementById('endereco').setAttribute('placeholder', 'Digite o endereço corretamente!');
+        }
+        if(vbairro == "")
+        {
+            document.getElementById('bairro').style.border = "1px solid red";
+            document.getElementById('bairro').setAttribute('placeholder', 'Digite o bairro corretamente!');
+        }
+        if(vsexo == "Selecione uma opção")
+        {
+            document.getElementById('sexo').style.border = "1px solid red";
+        }
+        if(vcidade == "")
+        {
+            document.getElementById('cidade').style.border = "1px solid red";
+            document.getElementById('cidade').setAttribute('placeholder', 'Digite a cidade corretamente!');
+        }
+        if(vestado == "Selecione uma opção")
+        {
+            document.getElementById('estado').style.border = "1px solid red";
+        }
+        if(vcep == "")
+        {
+            document.getElementById('cep').style.border = "1px solid red";
+            document.getElementById('cep').setAttribute('placeholder', 'Digite o CEP corretamente!');
+        }
+    }
 }
 
+var cont = 1;
+var div = 0;
 function adicionarItem (dados)
 {
     var tabela = document.getElementById('table');
@@ -235,27 +291,51 @@ function adicionarItem (dados)
             <th>Estado</th>
             <th>CEP</th>
         </tr>
-        
     `;
 
     for(let dados_paciente of dados)
     {
-        html += `
-            <tr >
-                <td data-th="Nome"style="margin-top: 50px">${dados_paciente.nome}</td>
-                <td data-th="Telefone">${dados_paciente.telefone}</td>
-                <td data-th="Email">${dados_paciente.email}</td>
-                <td data-th="Data de Nascimento">${dados_paciente.nascimento}</td>
-                <td data-th="CPF">${dados_paciente.cpf}</td>
-                <td data-th="Endereço">${dados_paciente.endereco}</td>
-                <td data-th="Bairro">${dados_paciente.bairro}</td>
-                <td data-th="Sexo">${dados_paciente.sexo}</td>
-                <td data-th="Cidade">${dados_paciente.cidade}</td>
-                <td data-th="Estado">${dados_paciente.estado}</td>
-                <td data-th="CEP">${dados_paciente.cep}</td>
+
+        div = cont % 2;
+        cont++;
+        console.log(div);
+        if(div == 0)
+        {
+            html += `
+            <tr class="tbody">
+                <td data-th="Nome" style="margin-top: 20px; background-color: #00b6a1;">${dados_paciente.nome}</td>
+                <td data-th="Telefone" style="background-color: #00b6a1;"">${dados_paciente.telefone}</td>
+                <td data-th="Email" style="background-color: #00b6a1;"">${dados_paciente.email}</td>
+                <td data-th="Data de Nascimento" style="background-color: #00b6a1;"">${dados_paciente.nascimento}</td>
+                <td data-th="CPF" style="background-color: #00b6a1;"">${dados_paciente.cpf}</td>
+                <td data-th="Endereço" style="background-color: #00b6a1;"">${dados_paciente.endereco}</td>
+                <td data-th="Bairro" style="background-color: #00b6a1;"">${dados_paciente.bairro}</td>
+                <td data-th="Sexo" style="background-color: #00b6a1;"">${dados_paciente.sexo}</td>
+                <td data-th="Cidade" style="background-color: #00b6a1;"">${dados_paciente.cidade}</td>
+                <td data-th="Estado" style="background-color: #00b6a1;"">${dados_paciente.estado}</td>
+                <td data-th="CEP" style="background-color: #00b6a1;"">${dados_paciente.cep}</td>
             </tr>
-            
-        `
+         `
+        }
+        else
+        {
+            html += `
+            <tr class="tbody">
+                <td data-th="Nome" style="margin-top: 20px; background-color: #43e6d39c;">${dados_paciente.nome}</td>
+                <td data-th="Telefone" style="background-color: #43e6d39c;">${dados_paciente.telefone}</td>
+                <td data-th="Email" style="background-color: #43e6d39c;">${dados_paciente.email}</td>
+                <td data-th="Data de Nascimento" style="background-color: #43e6d39c;">${dados_paciente.nascimento}</td>
+                <td data-th="CPF" style="background-color: #43e6d39c;">${dados_paciente.cpf}</td>
+                <td data-th="Endereço" style="background-color: #43e6d39c;">${dados_paciente.endereco}</td>
+                <td data-th="Bairro" style="background-color: #43e6d39c;">${dados_paciente.bairro}</td>
+                <td data-th="Sexo" style="background-color: #43e6d39c;">${dados_paciente.sexo}</td>
+                <td data-th="Cidade" style="background-color: #43e6d39c;">${dados_paciente.cidade}</td>
+                <td data-th="Estado" style="background-color: #43e6d39c;">${dados_paciente.estado}</td>
+                <td data-th="CEP" style="background-color: #43e6d39c;">${dados_paciente.cep}</td>
+            </tr>
+         `
+        }
+        
     }
     tabela.innerHTML = html;
 
